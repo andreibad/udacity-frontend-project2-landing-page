4fde2c08e0a9c7e78425e987c68e30b750837bc3
andrei <andrei.badulescu@gmail.com>
2020-03-28 01:16:58 -0700

    built dynamic nav, and implemented active class

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -24,7 +24,9 @@
  * Start Helper Functions
  * 
 */
-
+function sectionview() {
+    console.log('The section was viewed');
+}
 
 
 /**
@@ -34,11 +36,33 @@
 */
 
 // build the nav
-
+let navbarlist = document.querySelector("#navbar__list");
+let sections = document.querySelectorAll("[data-nav]");
+for (let section of sections) {
+      console.log(section);
+      let navitem = document.createElement("li");
+      let navlink = document.createElement("a");
+      navlink.innerText = section.getAttribute("data-nav");
+      navlink.setAttribute("href","#"+ section.getAttribute("data-nav"))
+      navlink.className = "menu__link";
+      navitem.appendChild(navlink)
+      navbarlist.appendChild(navitem);
+  }
 
 // Add class 'active' to section when near top of viewport
-
-
+function makeactive(event) {
+    console.log("scrolling");
+    for (let section of sections) {
+        var rect = section.getBoundingClientRect();
+        console.log(rect.top);
+        if (rect.top < 700 & rect.top > -120){
+            section.className = "your-active-class";
+        }
+        else{
+            section.className = "";
+        }
+    }
+}
 // Scroll to anchor ID using scrollTO event
 
 
@@ -53,5 +77,9 @@
 // Scroll to section on link click
 
 // Set sections as active
+let main = document.querySelector("main");
+console.log(main);
+document.addEventListener("scroll",makeactive);
+
 
 
